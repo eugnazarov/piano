4bc2335a3bf503a1aba9d3fae79b36bece9e86d3
eugene@git config --global user.name eugenegit config --global user.name eugene@ <eugnazaroff@gmail.com>
2021-04-07 20:17:51 +0400

    finished basic logic

--- src/components/key/Key.js
+++ src/components/key/Key.js
@@ -2,12 +2,28 @@ import './Key.scss'
 
 import React from 'react';
 
-const Key = ({color}) => {
-    return (
-        <div className={color === "white" ? "key key_white" : "key key_black"}>
+const Key = ({color, pitch}) => {
 
-        </div>
+    const playSound = () => {
+        let audio = new Audio(document.getElementById(`${pitch}`).src)
+        audio.play()
+
+
+    }
+
+    return (
+        <>
+            <audio
+                src={`../../src/${pitch}.wav`}
+                id={pitch}/>
+            <div
+                onMouseDown={playSound}
+                className={color === "white" ? "key key_white" : "key key_black"}/>
+
+        </>
     );
+
 }
+
 
 export default Key;
--- src/components/keyboard/keyboard.js
+++ src/components/keyboard/keyboard.js
@@ -5,18 +5,20 @@ import Key from "../key/Key";
 const Keyboard = () => {
     return (
         <div className="keyboard">
-            <Key color="white" />
-            <Key color="black" />
-            <Key color="white" />
-            <Key color="black" />
-            <Key color="white" />
-            <Key color="white" />
-            <Key color="black" />
-            <Key color="white" />
-            <Key color="black" />
-            <Key color="white" />
-            <Key color="black" />
-            <Key color="white" />
+            <Key color="white" pitch={"c"} />
+            <Key color="black" pitch={"c1"} />
+            <Key color="white" pitch={"d"}/>
+            <Key color="black" pitch={"d1"}/>
+            <Key color="white" pitch={"e"}/>
+            <Key color="white" pitch={"f"}/>
+            <Key color="black" pitch={"f1"}/>
+            <Key color="white" pitch={"g"}/>
+            <Key color="black" pitch={"g1"}/>
+            <Key color="white" pitch={"a"}/>
+            <Key color="black" pitch={"a1"}/>
+            <Key color="white" pitch={"h"}/>
+            <Key color="white" pitch={"cH"}/>
+
 
 
         </div>
